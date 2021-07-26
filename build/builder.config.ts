707c6f5fad2b69f2a1150815ee018303d29e1944
Lei Liu <leiliu@grandstream.cn>
2021-07-26 09:17:49 +0800

    update yaml

--- build/builder.config.ts
+++ build/builder.config.ts
@@ -26,7 +26,8 @@ const config: Configuration = {
   asar: false,
   directories: {
     buildResources: 'assets',
-    output: path.join(buildConfig.release, `${productName}-release-${version}.${buildVersion}`),
+    output: path.join(buildConfig.release),
+    // output: path.join(buildConfig.release, `${productName}-release-${version}.${buildVersion}`),
   },
   win: {
     icon: ICON_ICO,
